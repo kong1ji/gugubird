3fcf0df6f34a2b7ca1913fbbb159f9146c6e5846
chenzhihui <chen.zhihui6@iwhalecloud.com>
2020-11-24 11:52:45 +0800

    first commit
    (cherry picked from commit 913d5a1cba1a8360a5ef9e6fe14e9193498c1afd)

---
+++ src/main/java/com/minnan/gugubird/GugubirdApplication.java
@@ -0,0 +1,13 @@
+package com.minnan.gugubird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GugubirdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GugubirdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minnan/gugubird/GugubirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minnan.gugubird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GugubirdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
